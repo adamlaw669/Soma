58db20a24aa8c87c3f67bb27b0d54da1179fd13e
Adam <adamlawal669@gmail.com>
2025-09-07 13:29:03 +0100

    minor changes

--- frontend/.next/server/webpack-runtime.js
+++ frontend/.next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("f99b9bd12ae3374c")
+/******/ 		__webpack_require__.h = () => ("f15076bc841abf42")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
